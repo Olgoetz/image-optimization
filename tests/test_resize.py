769d5f8f53b06812a7c500c9452c245ad798cabb
Oliver Götz <olivergoetz@OliversAir2022.fritz.box>
2023-04-11 00:54:08 +0200

    module name fixed

--- tests/test_resize.py
+++ tests/test_resize.py
@@ -2,7 +2,7 @@ import unittest
 import pathlib
 import os
 from PIL import Image
-from app.model import resize
+from app import model
 # Current directory
 HERE = pathlib.Path(__file__).resolve().parent
 
@@ -13,24 +13,24 @@ class ApplicationTests(unittest.TestCase):
         output_path = os.path.join(f"{HERE}/pics_out", "")
 
         # Test fixed height
-        resize(input_path, output_path, fixed_height=500,
-               fixed_width=None, quality=90)
+        model.resize(input_path, output_path, fixed_height=500,
+                     fixed_width=None, quality=90)
 
         for img in os.listdir(output_path):
             height = Image.open(output_path+img).size[1]
             self.assertEqual(height, 500)
 
         # Test fixed width
-        resize(input_path, output_path, fixed_height=None,
-               fixed_width=300, quality=90)
+        model.resize(input_path, output_path, fixed_height=None,
+                     fixed_width=300, quality=90)
 
         for img in os.listdir(output_path):
             width = Image.open(output_path+img).size[0]
             self.assertEqual(width, 300)
 
         # Test fixed width AND fixed height
-        resize(input_path, output_path, fixed_height=400,
-               fixed_width=300, quality=90)
+        model.resize(input_path, output_path, fixed_height=400,
+                     fixed_width=300, quality=90)
 
         for img in os.listdir(output_path):
             width = Image.open(output_path+img).size[0]
